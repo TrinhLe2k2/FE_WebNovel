bb5d1ac012a63365c835aa388e5ec7c3dbac53e1
TrinhLe2k2 <khanhtrinh.phuyen@gmail.com>
2023-08-13 14:17:24 +0700

    fix BoLoc

--- Content/assets/js/BoLoc.js
+++ Content/assets/js/BoLoc.js
@@ -1,4 +1,5 @@
 var btnTabFilter = document.querySelector('#Main .FilterResults .Btn-Tab-Filter');
+var filterlabel = document.querySelector('#Main .FilterResults #FilterLabel');
 var filter = document.querySelector('#Main .FilterResults .Filter');
 
 
@@ -11,5 +12,13 @@ btnTabFilter.addEventListener('change', function() {
     }else {
         filter.classList.add('d-none');
         filter.classList.remove('CustomCssFilter');
+    }
+});
+
+document.addEventListener('click', function (event) {
+    if (!filter.contains(event.target) && !btnTabFilter.contains(event.target) && !filterlabel.contains(event.target)) {
+        filter.classList.add('d-none');
+        filter.classList.remove('CustomCssFilter');
+        btnTabFilter.checked = false;
     }
 });
